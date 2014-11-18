d5ad101304d2d91c261cfe665afe8fa5cb36f0eb
sakoh <sakohartounian@yahoo.com>
2014-11-18 10:59:14 -0800

    editing now works

--- public/js/controllers/edit.js
+++ public/js/controllers/edit.js
@@ -1,16 +1,22 @@
-angular.module('blue_media.edit_controller', ['restangular']).
+angular.module('blue_media.edit_controller', ['restangular','ngRoute']).
   controller('EditCtrl', function($scope, Restangular, $location, $routeParams){
+
     Restangular.one('users', $routeParams.id).get().then(function(user){
       $scope.user = user;
     });
 
     $scope.editUser = function(){
-      Restangular.one('users', $routeParams.id).then(function(user){
-        user.put().$object.then(function(){
-          return $location.path('/show/'+$routeParams.id);
-        }, function(err){
-          console.log(err);
+
+      Restangular.one('users', $routeParams.id).get().then(function(user){
+        user.name = $scope.user.name;
+        user.email = $scope.user.email;
+        user.password = $scope.user.password;
+
+        user.put().then(function(){
+          return $location.path('show/' + $routeParams.id);
         });
       });
+
     }
+
   });
